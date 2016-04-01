70adef0103683cefae7f0a504f50a2ad197891b7
Tom Peters <tpeters@synacor.com>
2016-04-01 14:40:51 -0400

    Initial commit

---
+++ lib/Test/MockPackages.pm
@@ -0,0 +1,109 @@
+package Test::MockPackages;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::MockPackages - The great new Test::MockPackages!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::MockPackages;
+
+    my $foo = Test::MockPackages->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Tom Peters, C<< <tpeters at synacor.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-mockpackages at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-MockPackages>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::MockPackages
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-MockPackages>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-MockPackages>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-MockPackages>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-MockPackages/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2016 Tom Peters.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1;    # End of Test::MockPackages
