36c1b9b280dbe39a2668afa70815ad955151afd3
Tom Peters <tpeters@synacor.com>
2016-04-13 11:19:43 -0400

    Adds exportable mock() subroutine

--- lib/Test/MockPackages.pm
+++ lib/Test/MockPackages.pm
@@ -2,12 +2,17 @@ package Test::MockPackages;
 use strict;
 use warnings;
 use utf8;
+use feature qw(state);
 
 our $VERSION = '0.1';
 
 use Carp qw(croak);
 use English qw(-no_match_vars);
+use Exporter qw(import);
+use Test::MockPackages::Mock();
 use Test::MockPackages::Package();
+
+our @EXPORT_OK = qw(mock);
 
 sub new {
     my ( $pkg ) = @ARG;
@@ -27,6 +32,70 @@ sub pkg {
     }
 
     return $self->{_packages}{$pkg_name} = Test::MockPackages::Package->new( $pkg_name );
+}
+
+sub mock {
+    my ( $config ) = @ARG;
+
+    _must_validate( $config );
+
+    # this while loop is similar to the one found in _must_validate, but I'm explicitly keeping them separate
+    # so that we don't end up with partially built and mocked subroutines and methods.
+    my $m = Test::MockPackages->new();
+    while ( my ( $pkg, $subs_href ) = each %$config ) {
+        my $mp = $m->pkg( $pkg );
+
+        while ( my ( $sub, $config_aref ) = each %$subs_href ) {
+            my $ms = $mp->mock( $sub );
+
+            for ( my $i = 0; $i < @$config_aref; $i += 2 ) {
+                my ( $mock_method, $args_aref ) = @$config_aref[ $i, $i + 1 ];
+
+                my $method = $ms->can( $mock_method );
+                $ms->$method( @$args_aref );
+            }
+        }
+    }
+
+    return $m;
+}
+
+sub _must_validate {
+    my ( $config ) = @ARG;
+
+    if ( ref( $config ) ne 'HASH' ) {
+        croak( 'config must be a HASH' );
+    }
+
+    while ( my ( $pkg, $subs_href ) = each %$config ) {
+        if ( ref( $subs_href ) ne 'HASH' ) {
+            croak( "value for $pkg must be a HASH" );
+        }
+
+        while ( my ( $sub, $config_aref ) = each %$subs_href ) {
+            if ( ref( $config_aref ) ne 'ARRAY' ) {
+                croak( "value for ${pkg}::$sub must be an ARRAY" );
+            }
+
+            if ( @$config_aref % 2 > 0 ) {
+                croak( "value for ${pkg}::$sub must be an even-sized ARRAY" );
+            }
+
+            for ( my $i = 0; $i < @$config_aref; $i += 2 ) {
+                my ( $mock_method, $args_aref ) = @$config_aref[ $i, $i + 1 ];
+
+                if ( ref( $args_aref ) ne 'ARRAY' ) {
+                    croak( "arguments must be an ARRAY for mock method $mock_method in ${pkg}::$sub" );
+                }
+
+                if ( !eval { Test::MockPackages::Mock->can( $mock_method ) } ) {
+                    croak( "$mock_method is not a capability of Test::MockPackages::Mock in ${pkg}::$sub" );
+                }
+            }
+        }
+    }
+
+    return 1;
 }
 
 1;
@@ -61,6 +130,29 @@ Test::MockPackages - Mocking framework
    ->expects( $arg3, $arg4, $arg5 )   # expects & returns for call #2
    ->returns( $retval2 );
 
+ # using a helper sub.
+ my $m = mock({
+     'ACME::Widget' => {
+         do_thing => [
+            expects => [ $arg1, $arg2 ],
+            returns => [ $retval ],
+         ],
+         dont_do_other_thing => [
+            never_called => [],
+         ],
+         do_multiple_things => [
+            is_method => [],
+            expects => [ $arg1, $arg2 ],
+            returns => [ $retval ],
+            expects => [ $arg3, $arg4, $arg5 ],
+            returns => [ $retval2 ],
+         ],
+     },
+     'ACME::ImprovedWidget' => {
+         ...
+     },
+ });
+
 =head1 CONSTRUCTOR
 
 =head2 new( )
@@ -90,6 +182,38 @@ Both this package, and L<Test::MockPackages::Package> are light-weight packages 
 Instantiates a new L<Test::MockPackages::Package> object using for C<$pkg_name>. Repeated calls to this method with the same C<$pkg_name> will return the same object.
 
 Return value: A L<Test::MockPackages::Package> object.
+
+=head1 EXPORTED SUBROUTINES
+
+=head2 mock( HashRef $configuration ) : Test::MockPackages
+
+C<mock()> is an exportable subroutine (not exported by default) that allows you to quickly configure your mocks in one call. Behind the scenes, it converts your C<$configuration> to standard OOP calls to the L<Test::MockPackages>, L<Test::MockPackages::Package>, and L<Test::MockPackages::Mock> packages.
+
+C<$configuration> expects the following structure:
+
+ {
+     $package_name => {
+         $sub_or_method_name => [
+            $option => [ 'arg1', ... ],
+         ],
+     }
+     ...
+ }
+
+C<$package_name> is the name of your package. This is equvalent to the call:
+
+ $m->pkg( $package_name )
+ 
+C<$sub_or_method_name> is the name of the subroutine or method that you'd like to mock. This is equivalent to:
+
+ $m->pkg( $package_name )
+   ->mock( $sub_or_method_name )
+
+C<$option> is the name of one of the methods you can call in L<Test::MockPackages::Mock> (e.g. C<called>, C<never_called>, C<is_method>, C<expects>, C<returns>). The value for C<$option> should always be an ArrayRef. This is equivalent to:
+
+ $m->pkg( $package_name )
+   ->mock( $sub_or_method_name )
+   ->$option( @{ [ 'arg1', ... ] } );
 
 =head1 SEE ALSO
 
